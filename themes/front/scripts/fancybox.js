30cd9153c60427e9c17f113f7511f156e4a67f96
woqiqi <1615391348@qq.com>
2020-01-06 15:24:06 +0800

    update

--- themes/front/scripts/fancybox.js
+++ themes/front/scripts/fancybox.js
@@ -1,5 +1,10 @@
 var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
 
+
+const cheerio = require('cheerio'); 
+
+const { basename } = require('path');
+
 /**
 * Fancybox tag
 *
@@ -21,4 +26,37 @@ hexo.extend.tag.register('fancybox', function(args){
     '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
     '</a>' +
     (title ? '<span class="caption">' + title + '</span>' : '');
+});
+
+hexo.extend.helper.register('page_anchor', str => {
+  const $ = cheerio.load(str, {decodeEntities: false});
+  const headings = $('h1, h2, h3, h4, h5, h6');
+  if (!headings.length) return str;
+  headings.each(function() {
+    const id = $(this).attr('id');
+
+    $(this)
+      .addClass('article-heading')
+      .append(`<a class="article-anchor" href="#${id}" aria-hidden="true"></a>`);
+  });
+
+  return $.html();
+});
+
+hexo.extend.helper.register('doc_sidebar', function(className) {
+  const type = this.page.canonical_path.split('/')[0];
+  const sidebar = this.site.data.sidebar[type];
+  // const path = basename(this.path);
+  let result = '';
+  const self = this;
+  const prefix = 'sidebar.' + type + '.';
+  const path = basename(this.path);
+  if (typeof sidebar === 'undefined') {
+    return '';
+  }
+  for (const [link, menu] of Object.entries(sidebar)) {
+    result += `<li class="hs-sidebar__item"><a href="${menu}" class="hs-sidebar__link ${menu==path?'active':''}" >${self.__(prefix + link)}</a></li>`;
+  }
+
+  return result;
 });
